32244003f7dd43e1ff3686dd3f0d4aeefcaa2610
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-04 20:34:50 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const commentRoute = "https://fistblog.onrender.com/posts/comment/";
 
 export const getPosts = async () => await axios.get(apiRoute);
 
-export const getSinglePost = async (id) => await axios.get(${apiRoute}${id});
+export const getSinglePost = async (id) => await axios.get(`${apiRoute}${id}`);
 
 export const createPost = async (post) => await axios.post(apiRoute, post);
 
