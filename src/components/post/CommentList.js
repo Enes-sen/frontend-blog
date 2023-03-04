1de40324c20bc7e21e0fea1c96532640e02ea994
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-04 11:58:37 +0300

    Create CommentList.js

---
+++ src/components/post/CommentList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
+import moment from "moment";
+
+const CommentList = ({ postId }) => {
+const comments = useSelector((state) =>
+state.comment.comments.filter((comment) => comment.post === postId)
+);
+
+const convertRelativeTime = (date) => {
+return moment(date).fromNow(); // tarihi "x zaman önce" formatında döndürür
+};
+
+return (
+<div className="comments">
+{comments.map((comment) => (
+<Card key={comment._id} className="mt-3">
+<CardBody>
+<CardTitle tag="h6">{comment.name}</CardTitle>
+<CardSubtitle tag="h6" className="mb-2 text-muted">
+{convertRelativeTime(comment.date)}
+</CardSubtitle>
+<p>{comment.content}</p>
+</CardBody>
+</Card>
+))}
+</div>
+);
+};
+
+export default CommentList;
