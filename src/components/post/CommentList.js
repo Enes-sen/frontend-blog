3a9ce4e284a80f9cfec5dbde6745871485e41f37
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-06 14:56:35 +0300

    Update CommentList.js

--- src/components/post/CommentList.js
+++ src/components/post/CommentList.js
@@ -4,30 +4,30 @@ import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
 import moment from "moment";
 
 const CommentList = ({ currentPost }) => {
-const comments = currentPost.comments;
+  const comments = currentPost ? currentPost.comments : [];
 
-// check if comments is defined before filtering
-const filteredComments = comments ? comments.filter(comment => comment.postId === currentPost._id) : [];
+  // check if comments is defined before filtering
+  const filteredComments = comments.filter(comment => comment.postId === currentPost._id);
 
-const convertRelativeTime = (date) => {
-return moment(date).fromNow();
-};
+  const convertRelativeTime = (date) => {
+    return moment(date).fromNow();
+  };
 
-return (
-<div className="comments">
-{filteredComments.map((comment) => (
-<Card key={comment._id} className="mt-3">
-<CardBody>
-<CardTitle tag="h6">{comment.name}</CardTitle>
-<CardSubtitle tag="h6" className="mb-2 text-muted">
-{convertRelativeTime(comment.date)}
-</CardSubtitle>
-<p>{comment.comment}</p>
-</CardBody>
-</Card>
-))}
-</div>
-);
+  return (
+    <div className="comments">
+      {filteredComments.map((comment) => (
+        <Card key={comment._id} className="mt-3">
+          <CardBody>
+            <CardTitle tag="h6">{comment.name}</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">
+              {convertRelativeTime(comment.date)}
+            </CardSubtitle>
+            <p>{comment.comment}</p>
+          </CardBody>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default CommentList;
