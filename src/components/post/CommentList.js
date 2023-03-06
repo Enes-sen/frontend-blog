5d7348c3e7981c0bcf5f30c86354bd07a84758fa
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-06 13:37:20 +0300

    Update CommentList.js

--- src/components/post/CommentList.js
+++ src/components/post/CommentList.js
@@ -3,30 +3,30 @@ import { useSelector } from "react-redux";
 import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
 import moment from "moment";
 
-const CommentList = ({ postId }) => {
-const comments = useSelector(state => state.posts);
+const CommentList = ({ currentPost }) => {
+  const comments = useSelector((state) =>
+    state.posts.find((post) => post._id === currentPost._id).comments
+  );
+  console.log(comments); 
+  const convertRelativeTime = (date) => {
+    return moment(date).fromNow();
+  };
 
-console.log(comments); // state yerine comments yazdım
-
-const convertRelativeTime = (date) => {
-return moment(date).fromNow(); // tarihi "x zaman önce" formatında döndürür
-};
-
-return (
-<div className="comments">
-{comments.map((comment) => (
-<Card key={comment._id} className="mt-3">
-<CardBody>
-<CardTitle tag="h6">{comment.name}</CardTitle>
-<CardSubtitle tag="h6" className="mb-2 text-muted">
-{convertRelativeTime(comment.date)}
-</CardSubtitle>
-<p>{comment.content}</p>
-</CardBody>
-</Card>
-))}
-</div>
-);
+  return (
+    <div className="comments">
+      {comments.map((comment) => (
+        <Card key={comment._id} className="mt-3">
+          <CardBody>
+            <CardTitle tag="h6">{comment.name}</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">
+              {convertRelativeTime(comment.date)}
+            </CardSubtitle>
+            <p>{comment.content}</p>
+          </CardBody>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default CommentList;
