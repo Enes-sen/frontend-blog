d221a67220ce432578d7926487977a4b94adc930
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-07 20:46:56 +0300

    Update CommentList.js

--- src/components/post/CommentList.js
+++ src/components/post/CommentList.js
@@ -5,43 +5,41 @@ import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
 import moment from "moment";
 
 const CommentList = ({ postId }) => {
-  const dispatch = useDispatch();
-  const comments = useSelector((state) => state.posts.currentPost?.comments);
-  console.log("comments-array:", comments);
+const dispatch = useDispatch();
+const comments = useSelector((state) => state.posts.currentPost?.comments);
 
-  useEffect(() => {
-    dispatch(fetchPostComments(postId));
-  }, [dispatch, postId]);
+useEffect(() => {
+dispatch(fetchPostComments(postId));
+}, [dispatch, postId]);
 
-  const convertRelativeTime = (date) => {
-    return moment(date).fromNow();
-  };
+const convertRelativeTime = (date) => {
+return moment(date).fromNow();
+};
 
-  const comments = useSelector((state) => state.posts.currentPost?.comments);
 const filteredComments = Array.isArray(comments) && comments.filter((comment) => comment.postId === postId);
 
 if (!Array.isArray(comments)) {
-  return <div>Loading comments...</div>;
+return <div>Loading comments...</div>;
 }
 
 if (!filteredComments.length) {
-  return <div>No comments yet.</div>;
+return <div>No comments yet.</div>;
 }
 
 return (
-  <div className="comments">
-    {filteredComments.map((comment) => (
-      <Card key={comment._id} className="mt-3">
-        <CardBody>
-          <CardTitle tag="h6">{comment.name}</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">
-            {convertRelativeTime(comment.date)}
-          </CardSubtitle>
-          <p>{comment.comment}</p>
-        </CardBody>
-      </Card>
-    ))}
-  </div>
+<div className="comments">
+{filteredComments.map((comment) => (
+<Card key={comment._id} className="mt-3">
+<CardBody>
+<CardTitle tag="h6">{comment.name}</CardTitle>
+<CardSubtitle tag="h6" className="mb-2 text-muted">
+{convertRelativeTime(comment.date)}
+</CardSubtitle>
+<p>{comment.comment}</p>
+</CardBody>
+</Card>
+))}
+</div>
 );
 };
 
