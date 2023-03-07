d5193cf5034ac827363d64712008f2937a7e5484
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-07 21:57:42 +0300

    Update CommentList.js

--- src/components/post/CommentList.js
+++ src/components/post/CommentList.js
@@ -5,41 +5,41 @@ import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
 import moment from "moment";
 
 const CommentList = ({ postId }) => {
-const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchPostComments(postId));
+  }, [dispatch, postId]);
 
+  const convertRelativeTime = (date) => {
+    return moment(date).fromNow();
+  };
 
-useEffect(() => {
-dispatch(fetchPostComments(postId));
-}, [dispatch, postId]);
+  const comments = useSelector((state) => state.posts.currentPost?.comments);
+  console.log(comments);
 
-const convertRelativeTime = (date) => {
-return moment(date).fromNow();
-};
-const comments = useSelector((state) => state.posts.currentPost?.comments);
- console.log(comments);
-if (!Array.isArray(comments)) {
-return <div>Loading comments...</div>;
-}
+  if (!Array.isArray(comments)) {
+    return <div>Loading comments...</div>;
+  }
 
-return (
-<div className="comments">
-{comments
-.filter((comment) => comment.postId === postId)
-.map((comment) => (
-<Card key={comment._id} className="mt-3">
-<CardBody>
-<CardTitle tag="h6">{comment.name}</CardTitle>
-<CardSubtitle tag="h6" className="mb-2 text-muted">
-{convertRelativeTime(comment.date)}
-</CardSubtitle>
-<p>{comment.comment}</p>
-</CardBody>
-</Card>
-))}
-{comments.length === 0 && <div>No comments yet.</div>}
-</div>
-);
+  return (
+    <div className="comments">
+      {comments
+        .filter((comment) => comment.postId === postId)
+        .map((comment) => (
+          <Card key={comment._id} className="mt-3">
+            <CardBody>
+              <CardTitle tag="h6">{comment.name}</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">
+                {convertRelativeTime(comment.date)}
+              </CardSubtitle>
+              <p>{comment.comment}</p>
+            </CardBody>
+          </Card>
+        ))}
+      {comments.length === 0 && <div>No comments yet.</div>}
+    </div>
+  );
 };
 
 export default CommentList;
