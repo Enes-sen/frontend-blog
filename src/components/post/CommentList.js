ed1aa2100d89386e7d41f8796d4e006fd71481bf
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-07 21:12:19 +0300

    Update CommentList.js

--- src/components/post/CommentList.js
+++ src/components/post/CommentList.js
@@ -5,42 +5,41 @@ import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
 import moment from "moment";
 
 const CommentList = ({ postId }) => {
-const dispatch = useDispatch();
-const comments = useSelector((state) => state.posts.currentPost?.comments);
-console.log(comments);
-useEffect(() => {
-dispatch(fetchPostComments(postId));
-}, [dispatch, postId]);
+  const dispatch = useDispatch();
+  const comments = useSelector((state) => state.posts.currentPost?.comments);
+  console.log(comments);
 
-const convertRelativeTime = (date) => {
-return moment(date).fromNow();
-};
+  useEffect(() => {
+    dispatch(fetchPostComments(postId));
+  }, [dispatch, postId]);
 
-const filteredComments = Array.isArray(comments) && comments.filter((comment) => comment.postId === postId);
+  const convertRelativeTime = (date) => {
+    return moment(date).fromNow();
+  };
 
-if (!Array.isArray(comments)) {
-return <div>Loading comments...</div>;
-}
+  if (!Array.isArray(comments)) {
+    return <div>Loading comments...</div>;
+  }
 
-if (!filteredComments.length) {
-return <div>No comments yet.</div>;
-}
+  if (!comments.length) {
+    return <div>No comments yet.</div>;
+  }
 
-return (
-<div className="comments">
-{filteredComments.map((comment) => (
-<Card key={comment._id} className="mt-3">
-<CardBody>
-<CardTitle tag="h6">{comment.name}</CardTitle>
-<CardSubtitle tag="h6" className="mb-2 text-muted">
-{convertRelativeTime(comment.date)}
-</CardSubtitle>
-<p>{comment.comment}</p>
-</CardBody>
-</Card>
-))}
-</div>
-);
+  return (
+    <div className="comments">
+      {comments.map((comment) => (
+        <Card key={comment._id} className="mt-3">
+          <CardBody>
+            <CardTitle tag="h6">{comment.name}</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">
+              {convertRelativeTime(comment.date)}
+            </CardSubtitle>
+            <p>{comment.comment}</p>
+          </CardBody>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default CommentList;
