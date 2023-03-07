96408b56f1c16cfaa4449ad28cdf48531adbe168
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-07 15:31:27 +0300

    Update SinglePost.js

--- src/components/post/SinglePost.js
+++ src/components/post/SinglePost.js
@@ -4,76 +4,76 @@ import { useParams } from "react-router-dom";
 import { fetchSinglePost, removePost } from "../../redux/actions/postActions";
 import moment from "moment";
 import {
-  Card,
-  CardBody,
-  CardHeader,
-  CardText,
-  Button,
-  CardImg,
-  Badge,
+Card,
+CardBody,
+CardHeader,
+CardText,
+Button,
+CardImg,
+Badge,
 } from "reactstrap";
 import noImage from "../../images/download.png";
 import CommentList from "./CommentList";
 
 const SinglePost = () => {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const currentPost = useSelector((state) => state.posts.currentPost);
-  const postId = id;
+const { id } = useParams();
+const dispatch = useDispatch();
+const currentPost = useSelector((state) => state.posts.currentPost);
+const postId = id;
 
-  useEffect(() => {
-    dispatch(fetchSinglePost(id));
-  }, [dispatch, id]);
+useEffect(() => {
+dispatch(fetchSinglePost(id));
+}, [dispatch, id]);
 
-  const convertRelativeTime = (date) => {
-    return moment(date).fromNow();
-  };
+const convertRelativeTime = (date) => {
+return moment(date).fromNow();
+};
 
-  const handleDelete = () => {
-    if (window.confirm("Bu gönderiyi silmek istediğinize emin misiniz?")) {
-      dispatch(removePost(currentPost._id));
-      window.location.href="/";
-    }
-  };
+const handleDelete = () => {
+if (window.confirm("Bu gönderiyi silmek istediğinize emin misiniz?")) {
+dispatch(removePost(currentPost._id));
+window.location.href="/";
+}
+};
 
-  if (!currentPost) {
-    return <div>Loading...</div>;
-  }
+if (!currentPost) {
+return <div>Loading...</div>;
+}
 
-  return (
-    <div style={{
-  display: "flex",
-  flex-direction: "column",
-  align-items: "center",
-  width: 100%;
-  max-width: 1200px; /* istediğiniz genişliği belirleyebilirsiniz */
-  margin: 0 auto;
-  padding: 0 15px;
+return (
+<div style={{
+display: "flex",
+flexDirection: "column",
+alignItems: "center",
+width: "100%",
+maxWidth: "1200px", /* istediğiniz genişliği belirleyebilirsiniz */
+margin: "0 auto",
+padding: "0 15px",
 }}>
-      <Card style={{ width: "90%", marginTop: "7%" }}>
-        <CardHeader tag={"h1"}>{currentPost.title}</CardHeader>
-        <CardImg alt="Card image cap" src={currentPost.image || noImage} style={{height:"60%",width:"70%" }}/>
-        <CardBody>
-          <Badge color="primary">
-            {convertRelativeTime(currentPost.date)}
-          </Badge>
-          <CardText>{currentPost.content}</CardText>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              marginTop: "20px",
-            }}
-          >
-            <Button color="danger" onClick={handleDelete}>
-              Kaldır
-            </Button>
-          </div>
-        </CardBody>
-      </Card>
-       <CommentList postId={postId} />
-    </div>
-  );
+<Card style={{ width: "90%", marginTop: "7%" }}>
+<CardHeader tag={"h1"}>{currentPost.title}</CardHeader>
+<CardImg alt="Card image cap" src={currentPost.image || noImage} style={{height:"60%",width:"70%" }}/>
+<CardBody>
+<Badge color="primary">
+{convertRelativeTime(currentPost.date)}
+</Badge>
+<CardText>{currentPost.content}</CardText>
+<div
+style={{
+display: "flex",
+justifyContent: "center",
+marginTop: "20px",
+}}
+>
+<Button color="danger" onClick={handleDelete}>
+Kaldır
+</Button>
+</div>
+</CardBody>
+</Card>
+<CommentList postId={postId} />
+</div>
+);
 };
 
 export default SinglePost;
