8f559efab3bb1f60bdb14c3e0eaed41317bbca1f
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-08 13:22:00 +0300

    Create CommentList.js

---
+++ src/components/post/CommentList.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPostComments } from "../../redux/actions/postActions";
+import { Card, CardBody, CardSubtitle, CardTitle } from "reactstrap";
+import moment from "moment";
+
+const CommentList = ({ postId }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPostComments(postId));
+  }, [dispatch, postId]);
+
+  const convertRelativeTime = (date) => {
+    return moment(date).fromNow();
+  };
+
+  const comments = useSelector((state) => state.posts.comments);
+  console.log(comments);
+
+  if (!Array.isArray(comments)) {
+    return <div>Loading comments...</div>;
+  }
+
+  return (
+    <div className="comments">
+      {comments
+        .sort((a, b) => new Date(b.date) - new Date(a.date)) // sort the comments by date
+        .map((comment) => (
+          <Card key={comment._id} className="mt-3">
+            <CardBody>
+              <CardTitle tag="h6">{comment.name}</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">
+                {convertRelativeTime(comment.date)}
+              </CardSubtitle>
+              <p>{comment.comment}</p>
+            </CardBody>
+          </Card>
+        ))}
+      {comments.length === 0 && <div>No comments yet.</div>}
+    </div>
+  );
+};
+export default CommentList;
