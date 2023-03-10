38174a423131885bf461427d20f1843bba815ba9
darklord <113107334+Enes-sen@users.noreply.github.com>
2023-03-10 15:48:14 +0300

    Update CommentList.js

--- src/components/post/CommentList.js
+++ src/components/post/CommentList.js
@@ -7,58 +7,69 @@ import moment from "moment";
 import "moment/locale/tr";
 
 const CommentList = ({ postId }) => {
-const dispatch = useDispatch();
-const comments = useSelector(state => state.posts.postComments);
+  const dispatch = useDispatch();
+  const comments = useSelector(state => state.posts.postComments);
 
-useEffect(() => {
-dispatch(fetchPostComments(postId));
-}, [dispatch, postId]);
+  useEffect(() => {
+    dispatch(fetchPostComments(postId));
+  }, [dispatch, postId]);
 
-const convertRelativeTime = (date) => {
-return moment(date).locale('tr').format('lll');
+  const convertRelativeTime = (date) => {
+    return moment(date).locale('tr').format('lll');
+  };
+
+  const handleDeleteComment = (commentId) => {
+    dispatch(removeComment(postId, commentId));
+  };
+
+  if (!comments) {
+    return <div>Loading comments...</div>;
+  }
+
+  return (
+    <div className="comments">
+      {comments.length > 0 ? (
+        comments
+          .sort((a, b) => new Date(b.date) - new Date(a.date))
+          .map((comment, index) => {
+            // Check if the comment is pending
+            const isPending = comment.status === 'pending';
+
+            return (
+              <React.Fragment key={index}>
+                <Card className={`mt-3 ${isPending ? 'bg-secondary text-white' : ''}`}>
+                  <CardBody>
+                    <CardTitle tag="h6">{comment?.name}</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">
+                      <Badge className="ml-2" color="secondary">{convertRelativeTime(comment?.date)}</Badge>
+                    </CardSubtitle>
+                    <CardText>{comment?.comment}</CardText>
+                    {isPending ? (
+                      <div className="text-center">
+                        <em>This comment is awaiting moderation.</em>
+                      </div>
+                    ) : (
+                      <Button color="danger" onClick={() => handleDeleteComment(comment._id)}>
+                        Delete Comment
+                      </Button>
+                    )}
+                  </CardBody>
+                </Card>
+                {index < comments.length - 1 && <div style={{ height: "50px" }}></div>}
+                {/* add space between posts */}
+              </React.Fragment>
+            )
+          })
+      ) : (
+        <div>No comments to show.</div>
+      )}
+      {console.log(comments)} {/* Yorumları Console'da görmek için bu satırı ekledik */}
+    </div>
+  );
 };
 
-const handleDeleteComment = (commentId) => {
-dispatch(removeComment(postId, commentId));
-};
-
-if (!comments) {
-return <div>Loading comments...</div>;
-}
-
-return (
-
-<div className="comments">
-{comments.length > 0 ? (
-comments
-.sort((a, b) => new Date(b.date) - new Date(a.date))
-.map((comment,index) => (
-<React.Fragment key={index}>
-<Card className="mt-3">
-<CardBody>
-<CardTitle tag="h6">{comment?.name}</CardTitle>
-<CardSubtitle tag="h6" className="mb-2 text-muted">
-<Badge className="ml-2" color="secondary">{convertRelativeTime(comment?.date)}</Badge>
-</CardSubtitle>
-<CardText>{comment?.comment}</CardText>
-<Button color="danger" onClick={() => handleDeleteComment(comment._id)}>
-Delete Comment
-</Button>
-</CardBody>
-</Card>
-{index < comments.length - 1 && <div style={{ height: "50px" }}></div>}
-{/* add space between posts */}
-</React.Fragment>
-))
-) : (
-<div>No comments to show.</div>
-)}
-{console.log(comments)} {/* Yorumları Console'da görmek için bu satırı ekledik */}
-</div>
-);
-};
 CommentList.propTypes = {
-postId: PropTypes.string.isRequired,
+  postId: PropTypes.string.isRequired,
 };
 
 export default CommentList;
